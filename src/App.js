b9cf905a6ecd8c27295513793fdcbc1aeff6e712
Abhinav <ajagekarajagekar@gmail.com>
2023-07-07 15:18:46 +0530

    Lifting state from MovieList to App.js

--- src/App.js
+++ src/App.js
@@ -2,15 +2,83 @@ import MovieList from "./MovieList";
 import Student from "./Student";
 import Navbar from "./Navbar";
 import React from "react";
+import { movies } from "./moviesData";
+
 
 class App extends React.Component {
+  constructor(){
+    super();
+    this.state = {
+       movies: movies,
+       cartCount: 0,
+    }
+    // this.addStars = this.addStars.bind(this);
+    // this.removeStar = this.removeStar.bind(this);
+}
+
+handleIncStar = (movie) =>{
+    const {movies} = this.state;
+    const mid = movies.indexOf(movie);
+
+    if(movies[mid].star >=5){
+        return;
+    }
+    movies[mid].star += 0.5
+
+    this.setState({
+        movies: movies
+    });
+}
+
+handleDecStar = (movie) =>{
+    const {movies} = this.state;
+    const mid = movies.indexOf(movie);
+
+    if(movies[mid].star <= 0){
+        return;
+    }
+    movies[mid].star -= 0.5
+
+    this.setState({
+        movies: movies
+    });
+}
+
+handleFav = (movie) =>{
+    const {movies} = this.state;
+    const mid = movies.indexOf(movie);
+
+    movies[mid].fav = !movies[mid].fav;
+
+    this.setState({
+        movies:movies
+    });
+    
+}
+
+handleCart = (movie) =>{
+    const{movies} = this.state;
+    const mid = movies.indexOf(movie);
+
+    movies[mid].isInCart = !movies[mid].isInCart;
+    this.setState({
+        movies:movies
+    });
+}
+
 
   render (){
+    const {movies} = this.state;
     return (
       <>
         <Navbar />
         {/* <h1>Movie App</h1>  */}
-        <MovieList />
+        <MovieList movies = {movies} 
+                  addStars = {this.handleIncStar} 
+                  removeStar = {this.handleDecStar} 
+                  handleFav = {this.handleFav} 
+                  handleCart ={this.handleCart} 
+                  />
         {/* <Student name="Alexa" marks={10} />
         <Student name="Siri" marks={20}/>
         <Student name="Google" marks={30}/>
--- src/MovieList.js
+++ src/MovieList.js
@@ -1,77 +1,17 @@
 import {Component} from "react";
 import MovieCard from "./Moviecard";
-import { movies } from "./moviesdata";
 
 class MovieList extends Component{
-    constructor(){
-        super();
-        this.state = {
-           movies: movies,
-           cartCount: 0,
-        }
-        // this.addStars = this.addStars.bind(this);
-        // this.removeStar = this.removeStar.bind(this);
-    }
-
-    handleIncStar = (movie) =>{
-        const {movies} = this.state;
-        const mid = movies.indexOf(movie);
-
-        if(movies[mid].star >=5){
-            return;
-        }
-        movies[mid].star += 0.5
-
-        this.setState({
-            movies: movies
-        });
-    }
-
-    handleDecStar = (movie) =>{
-        const {movies} = this.state;
-        const mid = movies.indexOf(movie);
-
-        if(movies[mid].star <= 0){
-            return;
-        }
-        movies[mid].star -= 0.5
-
-        this.setState({
-            movies: movies
-        });
-    }
-
-    handleFav = (movie) =>{
-        const {movies} = this.state;
-        const mid = movies.indexOf(movie);
-
-        movies[mid].fav = !movies[mid].fav;
-
-        this.setState({
-            movies:movies
-        });
-        
-    }
-
-    handleCart = (movie) =>{
-        const{movies} = this.state;
-        const mid = movies.indexOf(movie);
-
-        movies[mid].isInCart = !movies[mid].isInCart;
-        this.setState({
-            movies:movies
-        });
-    }
-    render(){
+        render(){
         // const {title,plot,price,rating,stars,fav,isIncart,poster} = this.state.movies;
-        const {movies} = this.state;
+        const {movies, addStars, removeStar, handleFav, handleCart} = this.props;
         return(
             <>
                 {movies.map((movie) => <MovieCard   movies = {movie} 
-                                                    addStars = {this.handleIncStar} 
-                                                    removeStar = {this.handleDecStar} 
-                                                    handleFav = {this.handleFav} 
-                                                    handleCart ={this.handleCart} 
+                                                    addStars = {addStars} 
+                                                    removeStar = {removeStar} 
+                                                    handleFav = {handleFav} 
+                                                    handleCart ={handleCart} 
                                                     key = {movie.id}/>)}
                 
             </>
